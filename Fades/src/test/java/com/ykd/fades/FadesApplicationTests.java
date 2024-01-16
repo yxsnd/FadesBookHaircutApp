0ffd99c77ce08e888940aeccbeb90fb6a15a4097
yxsnd <yasindu.d@hotmail.com>
2024-01-16 17:31:08 -0500

    updated backend springboot files

---
+++ Fades/src/test/java/com/ykd/fades/FadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykd.fades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
